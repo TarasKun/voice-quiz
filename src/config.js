533d89004d5c839fd724f813de752ae3842783cb
tkunyk <Vfvfvbkfhfve!66>
2021-02-11 21:45:46 +0200

    add config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config =  {
+    apiKey: "AIzaSyCuAIkGU9nZ8w1UDD4TfGRlm_zmP8959l8",
+    discoveryDocs:
+        ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    spreadsheetId: "19zwBMglHgIGzTVPdjVETptEVGkTB9oAFnErAG58vHag",
+    scop: "https://www.googleapis.com/auth/spreadsheets",
+    clientId: '246384217786-e6nqf7md1i07ut3ufnsbtmdkp51c64a7.apps.googleusercontent.com'
+};
+
+export default config;
