d9e57fe356620b33b5e0367738e891ebefe3249f
tkunyk <Vfvfvbkfhfve!66>
2021-02-27 15:56:37 +0200

    refuctor timer, remove .idea

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     const { REACT_APP_CLIENT_ID, REACT_APP_SCOPE } = process.env;
 
     window.gapi.load("client:auth2", function() {
-    window.gapi.auth2.init({client_id: REACT_APP_CLIENT_ID, scope: REACT_APP_SCOPE});
-});
+    window.gapi.auth2.init({client_id: REACT_APP_CLIENT_ID, scope: REACT_APP_SCOPE})});
+
   return (
     <div className="App center">
         <SignIn />
--- src/component/timer/Timer.js
+++ src/component/timer/Timer.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import Timer from 'react-compound-timer'
+import React, {useState, useEffect} from 'react';
 
 const CompoundTimer = ({timesUpHandler}) => {
+    const [counter, setCounter] = useState(180);
+
+    const timesUp = () => {
+        timesUpHandler();
+        return "Час вийшов";
+    }
+
+    const counterFormat = () => {
+        let min = Math.floor(counter / 60);
+        let sec = counter % 60;
+        if (sec < 10) {
+            sec = `0${sec}`
+        }
+
+        return `${min} : ${sec}`
+    }
+
+    useEffect(() => {
+        counter >= 1 && setTimeout(() => setCounter(counter - 1), 1000);
+    }, [counter]);
+
     return (
-        <Timer
-            initialTime={180_000}
-            direction="backward"
-            checkpoints={[
-                {
-                    time: 0,
-                    callback: () => timesUpHandler(),
-                }
-            ]}
-        >
-            {() => (
-                <div className='timer'>
-                    <Timer.Minutes/>:
-                    <Timer.Seconds/>
-                </div>
-            )}
-        </Timer>
-    )
+        <div>
+            {counter === 0 ? timesUp() : counterFormat()}
+        </div>
+    );
 }
 
 export default CompoundTimer;
